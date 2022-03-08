7539766c5c119ca27c1c2c9b3a151d1537d027c1
Shailesh Adhikari <shailesh@rasello.com>
2022-03-08 14:57:02 +0545

    new api endpoint designs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+import json
+from flask import Flask, render_template, request, redirect, url_for, Response
 from utils import *
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -11,15 +11,13 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-
 try:
-    # get_db_connection = psycopg2.connect(host=HOST, port=PORT, database=DATABASE,
-    #                                      user='jkddordfkdcbiu', password=PASSWORD)
     db = SQLAlchemy(app)
     get_db_connection = db.create_engine(SQLALCHEMY_DATABASE_URI,{})
     print("Successfully Connected to Database !")
 except Exception as e:
     print("CONNECTION ERROR",e)
+    raise ConnectionError("Could not connect to database")
 
 res = 'Waiting to Add !'
 
@@ -27,11 +25,22 @@ res = 'Waiting to Add !'
 def youtube_frame(res='Successful'):
     return render_template("frame.html", remarks=res)
 
-@app.route("/playlist", methods=['GET'])
+@app.route("/playlist", methods=['GET','POST'])
 def playlist():
     if request.method == 'GET':
-        result = get_table_initial_entry(get_db_connection)
+        result = initial_table_getall(get_db_connection)
         return to_json(result)
+
+    if request.method == 'POST':
+        data = request.get_json()
+        vId = data['id']
+        vName = data['name']
+        vThumb = data['thumbnail']
+        vDuration = data['duration']
+
+        result = add_to_initial_entry(get_db_connection, vId, vName, vDuration, vThumb)
+
+        return Response(result)
 
 @app.route("/playing", methods=['GET'])
 def playing():
@@ -39,123 +48,144 @@ def playing():
         result = table_playing(get_db_connection)
         return to_json(result)
 
-@app.route("/", methods=['POST', 'GET'])
-def search_add():
-    global res
-    table_data = get_table_initial_entry(get_db_connection)
-    playing = get_table_playing(get_db_connection)
-
-    if request.method == 'GET':
-        return render_template("search.html", response='',
-                               status=res, songl=table_data,
-                               playing = playing)
+@app.route("/search",methods=['POST'])
+def search():
     if request.method == 'POST':
-        searchQ = request.form['search']
-        vLink= request.form['vLink']
-
-        if (len(searchQ) | len(vLink)) == 0:
-            response = "Please enter 'SEARCH STRING' of 'VIDEO URL' !"
-        elif len(vLink) == 0:
-            response = get_search_results(searchQ) if len(searchQ) > 3 else 'Query should have at least 3 words !'
-        elif len(searchQ) == 0:
-            vId = return_vid(vLink)
-            response = 'Invalid Video Link !' if vId == "INVALID URL" else get_video_name(vId)
-        else:
-            response = "I don't know what happen !"
-
-        res = 'Successfully Added Last One !'
-        # return redirect(url_for('search_add',response = response))
-        return render_template("search.html", response=response,
-                               songl=table_data, status='Click To Add !',
-                               playing = playing)
-
-@app.route("/addToDatabase")
-def get_url_from_user():
-    v_id = request.args.get('videoId')
-    title = request.args.get('title')
-    print('ADDED',v_id,title)
-
-    db_update = add_to_initial_entry(get_db_connection,v_id,title)
-    print(db_update)
-
-    return redirect(url_for('search_add'))
-
-@app.route("/add")
-def add_songs():
-    v_id = request.args.get('videoId')
-    title = request.args.get('title')
-    print('ADDED',v_id,title)
-
-    db_update = add_to_initial_entry(get_db_connection,v_id,title)
-    print(db_update)
-
-    return "True" if db_update else "False"
+        query = request.args.get('q')
+        result = get_search_results(query)
+        return Response(json.dumps(result,default=str),
+                        mimetype='application/json')
 
 
-@app.route("/firstUpdate", methods=['POST'])
-def onPlayerReady():
-    print("Here at First Update")
-    return "OK"
+@app.route("/link",methods=['POST'])
+def link():
+    if request.method == 'POST':
+        query = request.args.get('link')
+        result = get_video_name(return_vid(query))
+        return Response(json.dumps(result,default=str),
+                        mimetype='application/json')
 
+@app.route("/getMostPlayed", methods=['GET'])
+def get_most_played():
+    if request.method == 'GET':
+        result = get_most_played(get_db_connection)
+        return to_json(result)
 
-@app.route("/endUpdate", methods=['POST', 'GET'])
-def onPlayerEnd():
-    print("Here at Last Update")
-
+@app.route("/end", methods=['GET'])
+def on_player_end():
     try:
         truncate(get_db_connection,'DELETE FROM playing')
     except Exception as e:
         print("Issue with Truncate !")
 
-    # if len(result) == 0:
-    #     return render_template("frame.html", remarks='Not any video in initial table !')
-    #
-    # remove_entry(""" DELETE FROM initial_entry WHERE id = ?""", result[0][0])
-    #
-    # if len(result) == 0:
-    #     return render_template("frame.html", remarks='Not any video in playing table !')
+    initial_result = initial_table_gettop(get_db_connection)
 
-    to_play = []
-    initial_result = get_table_initial_entry(get_db_connection)
-    if len(initial_result) == 0:
+    if initial_result is None:
         videos = get_from_already_played(get_db_connection)
-        if len(videos) <= 0:
+        if videos is None:
             return 'TIME TO ADD VIDEOS ! !'
 
-        print(videos)
-        to_play.extend((videos[0][0], videos[0][1]))
+        videos = dict(videos)
+        add_to_playing(get_db_connection, videos['id'], videos['name'],
+                       videos['duration'],videos['thumbnail'])
 
-        add_to_playing(get_db_connection, to_play[0], to_play[1])
+        result = dict(table_playing(get_db_connection))
 
-        result = get_table_playing(get_db_connection)
-        to_return = [result[-1][1], result[-1][2]]
-
-        update_already_played(get_db_connection, to_play[0])
+        update_already_played(get_db_connection, result['id'])
 
     else:
-        to_play.extend((initial_result[0][1], initial_result[0][4]))
-        remove_entry(get_db_connection, 'DELETE FROM initial_entry WHERE id=%s', initial_result[0][0])
+        initial_result = dict(initial_result)
+        remove_entry(get_db_connection, 'DELETE FROM initial_entry WHERE video_id=%s', initial_result['id'])
 
-        add_to_playing(get_db_connection,to_play[0],to_play[1])
+        add_to_playing(get_db_connection,initial_result['id'], initial_result['name'],
+                       initial_result['duration'],initial_result['thumbnail'])
 
-        result = get_table_playing(get_db_connection)
-        to_return = [result[-1][1],result[-1][2]]
+        result = dict(table_playing(get_db_connection))
 
-        add_to_already_played(get_db_connection,to_return[0],to_return[1])
+        add_to_already_played(get_db_connection,result['id'], result['name'],
+                              result['duration'],result['thumbnail'])
 
-    return to_return[0]
+    return result['id']
 
-# Delete duplicate entries from already_played table
-@app.route("/deleteDuplicate")
-def deletDuplicates():
-    print("Deleting Duplicates")
-    try:
-        connection = get_db_connection
-        sql = """ DELETE FROM already_played a USING already_played b
-                  WHERE a.id < b.id AND a.video_id = b.video_id AND a.video_name = b.video_name;"""
-        connection.execute(sql)
-        result = "SUCCESSFUL"
-    except Exception as e:
-        print("DELETE ERROR",e)
-        result = "DELETE ERROR"
-    return result
+# _______________________________________________________________
+
+# @app.route("/", methods=['POST', 'GET'])
+# def search_add():
+#     global res
+#     table_data = get_table_initial_entry(get_db_connection)
+#     playing = get_table_playing(get_db_connection)
+#
+#     if request.method == 'GET':
+#         return render_template("search.html", response='',
+#                                status=res, songl=table_data,
+#                                playing = playing)
+#     if request.method == 'POST':
+#         searchQ = request.form['search']
+#         vLink= request.form['vLink']
+#
+#         if (len(searchQ) | len(vLink)) == 0:
+#             response = "Please enter 'SEARCH STRING' of 'VIDEO URL' !"
+#         elif len(vLink) == 0:
+#             response = get_search_results(searchQ) if len(searchQ) > 3 else 'Query should have at least 3 words !'
+#         elif len(searchQ) == 0:
+#             vId = return_vid(vLink)
+#             response = 'Invalid Video Link !' if vId == "INVALID URL" else get_video_name(vId)
+#         else:
+#             response = "I don't know what happen !"
+#
+#         res = 'Successfully Added Last One !'
+#         # return redirect(url_for('search_add',response = response))
+#         return render_template("search.html", response=response,
+#                                songl=table_data, status='Click To Add !',
+#                                playing = playing)
+#
+# @app.route("/addToDatabase")
+# def get_url_from_user():
+#     v_id = request.args.get('videoId')
+#     title = request.args.get('title')
+#     print('ADDED',v_id,title)
+#
+#     db_update = add_to_initial_entry(get_db_connection,v_id,title)
+#     print(db_update)
+#
+#     return redirect(url_for('search_add'))
+#
+#
+# @app.route("/endUpdate", methods=['POST', 'GET'])
+# def onPlayerEnd():
+#     print("Here at Last Update")
+#
+#     try:
+#         truncate(get_db_connection,'DELETE FROM playing')
+#     except Exception as e:
+#         print("Issue with Truncate !")
+#
+#     to_play = []
+#     initial_result = get_table_initial_entry(get_db_connection)
+#     if len(initial_result) == 0:
+#         videos = get_from_already_played(get_db_connection)
+#         if len(videos) <= 0:
+#             return 'TIME TO ADD VIDEOS ! !'
+#
+#         print(videos)
+#         to_play.extend((videos[0][0], videos[0][1]))
+#
+#         add_to_playing(get_db_connection, to_play[0], to_play[1])
+#
+#         result = get_table_playing(get_db_connection)
+#         to_return = [result[-1][1], result[-1][2]]
+#
+#         update_already_played(get_db_connection, to_play[0])
+#
+#     else:
+#         to_play.extend((initial_result[0][1], initial_result[0][4]))
+#         remove_entry(get_db_connection, 'DELETE FROM initial_entry WHERE id=%s', initial_result[0][0])
+#
+#         add_to_playing(get_db_connection,to_play[0],to_play[1])
+#
+#         result = get_table_playing(get_db_connection)
+#         to_return = [result[-1][1],result[-1][2]]
+#
+#         add_to_already_played(get_db_connection,to_return[0],to_return[1])
+#
+#     return to_return[0]
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import os
 import re
-
+import isodate
 from dotenv import load_dotenv
 from flask import jsonify
 from googleapiclient.discovery import build
@@ -16,6 +16,10 @@ reg1 = "(https:\/\/youtu.be\/)[A-Za-z0-9-_]{11}"
 reg2 = "(https:\/\/www.youtube.com\/watch\?v=)[A-Za-z0-9-_]{11}"
 reg3 = "(https:\/\/www.youtube.com\/watch\?v=)[A-Za-z0-9-_]{11}(&ab_channel=)[A-Za-z0-9-_]+"
 
+def to_json(data):
+    return jsonify([dict(r) for r in data])
+
+# Checking url with regex and return video_id
 def return_vid(vURL):
     p1 = re.compile(reg1)
     p2 = re.compile(reg2)
@@ -28,150 +32,202 @@ def return_vid(vURL):
     elif p3.match(vURL):
         return p3.match(vURL)[0].split('=')[1]
     else:
-        return "INVALID URL"
+        raise ValueError('Invalid URL:"{}", please check the URL'.format(vURL))
 
-def update_data_entry(get_db_connection,sql,task):
+# Database operations (update, delete and turnucate)
+def update_data_entry(get_db_connection, sql, task):
+    try:
+        connection = get_db_connection
+        connection.execute(sql, task)
+        result = {'status':True,
+                  'message': 'Successfully updated'}
+    except Exception as e:
+        print("UPDATE ERROR", e)
+        result = {'status':False,
+                  'error': str(e)}
+
+    return result
+
+def remove_entry(get_db_connection, sql, data):
+    try:
+        connection = get_db_connection
+        connection.execute(sql, (data,))
+        result = {'status': True,
+                  'message': 'Successfully deleted'}
+    except Exception as e:
+        print("DELETE ERROR", e)
+        result = {'status': False,
+                  'error': str(e)}
+
+    return result
+
+def truncate(get_db_connection, sql):
     result = False
     try:
         connection = get_db_connection
-        # cur = connection.cursor()
-        connection.execute(sql,task)
-        # connection.commit()
-        # cur.close()
+        connection.execute(sql)
         result = True
     except Exception as e:
-        print("UPDATE ERROR",e)
+        print("TRUNCATE ERROR", e)
         result = False
 
     return result
 
-def get_table_initial_entry(get_db_connection):
-
-    sql=""" SELECT * FROM initial_entry ORDER BY updated_at ASC """
-    return get_db_connection.execute(sql).fetchall()
-
-def initial_table_entry(get_db_connection):
-    sql=""" SELECT * FROM initial_entry ORDER BY updated_at ASC """
+# Initial table operations
+def initial_table_getall(get_db_connection):
+    sql = """ SELECT video_id as id,name,thumbnail,duration,updated_at,updated_by
+              FROM initial_entry ORDER BY updated_at ASC """
     return get_db_connection.execute(sql).mappings().fetchall()
 
-def get_table_playing(get_db_connection):
+def initial_table_gettop(get_db_connection):
+    sql = """ SELECT video_id as id,name,thumbnail,duration,updated_at,updated_by
+              FROM initial_entry ORDER BY updated_at ASC """
+    return get_db_connection.execute(sql).mappings().first()
 
-    sql=""" SELECT * FROM playing"""
-    connection = get_db_connection
-    return connection.execute(sql).fetchall()
+def add_to_initial_entry(get_db_connection, v_id,
+                         v_title, v_time=0, v_thum='a'):
+    print("PARAMETER", v_id, v_title)
 
-def table_playing(get_db_connection):
-    sql=""" SELECT * FROM playing"""
-    return get_db_connection.execute(sql).mappings().fetchall()
-
-def get_api_connection():
-    return build(api_service_name, api_version, developerKey=API)
-
-def get_search_results(query):
-    request = get_api_connection().search().list(
-        part="snippet",
-        q=query,
-        maxResults=10,
-        type="video",
-        fields="items(etag,id/videoId,snippet(title,thumbnails/default))"
-    )
-    response = request.execute()
-
-    return  [{
-            "vid":each['id']['videoId'],
-            "vt":each['snippet']['title'],
-            "th":each['snippet']['thumbnails']['default']['url'],
-        } for each in response['items']]
-
-def get_video_name(vid):
-    request = get_api_connection().videos().list(
-        part="snippet",
-        id=vid,
-        fields="items(etag,snippet(title,thumbnails/default))"
-    )
-    response = request.execute()
-
-    return [{
-            "vid":vid,
-            "vt":each['snippet']['title'],
-            "th":each['snippet']['thumbnails']['default']['url'],
-        } for each in response['items']]
-
-def add_to_playing(get_db_connection,video_id,video_name):
-    print("PARAMETER",video_id,video_name)
-
-    sql = """ INSERT INTO playing (video,video_name) VALUES (%s,%s)"""
-    db_update = update_data_entry(get_db_connection,sql, (video_id,video_name))
-    print(db_update)
-
-    return "OK"
-
-def add_to_initial_entry(get_db_connection,v_id,title):
-    print("PARAMETER",v_id,title)
-
-    sql = """ INSERT INTO initial_entry (video_id,video_name) VALUES (%s,%s)"""
-    db_update = update_data_entry(get_db_connection,sql, (v_id,title))
+    sql = """ INSERT INTO initial_entry (video_id,name,duration,thumbnail) VALUES (%s,%s,%s,%s)"""
+    db_update = update_data_entry(get_db_connection, sql, (v_id, v_title,v_time,v_thum))
     print(db_update)
 
     return db_update
 
-def add_to_already_played(get_db_connection,v_id,v_name):
-    print("PARAMETER",v_id,v_name)
+# Playing table operations
+def table_playing(get_db_connection):
+    sql = """ SELECT video_id as id,name,thumbnail,duration
+              FROM playing"""
+    return get_db_connection.execute(sql).mappings().first()
 
-    sql = """ INSERT INTO already_played (video_id,video_name) VALUES (%s,%s)"""
-    db_update = update_data_entry(get_db_connection,sql, (v_id,v_name))
+def add_to_playing(get_db_connection, video_id, name,duration,thumbnail):
+    print("PARAMETER", video_id, name,duration,thumbnail)
+
+    sql = """ INSERT INTO playing (video_id,name,duration,thumbnail) VALUES (%s,%s,%s,%s)"""
+    db_update = update_data_entry(get_db_connection, sql, (video_id, name,duration,thumbnail))
     print(db_update)
 
     return "OK"
 
-def update_already_played(get_db_connection,v_id):
-    print("PARAMETER",v_id)
 
-    sql = """ UPDATE already_played
-    SET completed_on = CURRENT_TIMESTAMP
-    WHERE video_id=(%s)"""
-    db_update = update_data_entry(get_db_connection,sql, (v_id,))
-    print(db_update)
-
-    return "OK"
-
-def remove_entry(get_db_connection,sql,data):
-    result = False
-    try:
-        connection = get_db_connection
-        # cur = connection.cursor()
-        connection.execute(sql,(data,))
-        # connection.commit()
-        # cur.close()
-        result = True
-    except Exception as e:
-        print("DELETE ERROR",e)
-        result = False
-
-    return result
-
-def truncate(get_db_connection,sql):
-    result = False
-    try:
-        connection = get_db_connection
-        # cur = connection.cursor()
-        connection.execute(sql)
-        # connection.commit()
-        # cur.close()
-        result = True
-    except Exception as e:
-        print("TRUNCATE ERROR",e)
-        result = False
-
-    return result
-
-def get_from_already_played(get_db_connection):
-    sql = """SELECT video_id, video_name ,completed_on
+# Already played table operations
+def get_most_played(get_db_connection):
+    sql = """SELECT video_id as id, name ,duration, thumbnail, played AS count
              FROM already_played 
-             WHERE completed_on < (CURRENT_TIMESTAMP - 90 * INTERVAL '1 MINUTE')
-             ORDER BY completed_on ASC LIMIT 15"""
+             ORDER BY count ASC LIMIT 5"""
 
     return get_db_connection.execute(sql).fetchall()
 
-def to_json(data):
-    return jsonify([dict(r) for r in data])
+def get_from_already_played(get_db_connection):
+    sql = """SELECT video_id as id, name ,updated_at, thumbnail, duration
+             FROM already_played 
+             WHERE updated_at < (CURRENT_TIMESTAMP - 90 * INTERVAL '1 MINUTE')
+             ORDER BY updated_at ASC LIMIT 15"""
+
+    return get_db_connection.execute(sql).mappings().first()
+
+def add_to_already_played(get_db_connection, video_id, name,duration,thumbnail):
+    print("PARAMETER", video_id, name,duration,thumbnail)
+
+    sql = """ INSERT INTO already_played (video_id,name,duration,thumbnail) VALUES (%s,%s,%s,%s)"""
+    db_update = update_data_entry(get_db_connection, sql, (video_id, name,duration,thumbnail))
+    print(db_update)
+
+    return "OK"
+
+def update_already_played(get_db_connection, v_id):
+    print("PARAMETER", v_id)
+
+    sql = """ UPDATE already_played
+    SET completed_on = CURRENT_TIMESTAMP, played = played + 1
+    WHERE video_id=(%s)"""
+    db_update = update_data_entry(get_db_connection, sql, (v_id,))
+    print(db_update)
+
+    return "OK"
+
+# Youtube Api Operations
+def get_api_connection():
+    return build(api_service_name, api_version, developerKey=API)
+
+def get_search_results(query):
+    try:
+        request = get_api_connection().search().list(
+            part="snippet",
+            q=query,
+            maxResults=10,
+            type="video",
+            fields="items(etag,id/videoId,snippet(title,thumbnails/default))"
+        )
+        response = request.execute()
+        video_id = [each['id']['videoId'] for each in response['items']]
+        mapper = get_video_duration(video_id)
+
+        for item in response['items']:
+            for m in mapper:
+                if item['id']['videoId'] == m['id']:
+                    item['duration'] = isodate.parse_duration(m['duration'])
+
+        return [{
+            "vid": each['id']['videoId'],
+            "vt": each['snippet']['title'],
+            "vd": each['duration'],
+            "th": each['snippet']['thumbnails']['default']['url'],
+        } for each in response['items']]
+    except Exception as e:
+        return ("SEARCH ERROR", e)
+
+def get_video_name(vid):
+    try:
+        request = get_api_connection().videos().list(
+            part="snippet,contentDetails",
+            id=vid,
+            fields="items(etag,snippet(title,thumbnails/default),contentDetails(duration))"
+        )
+        response = request.execute()
+
+        return [{
+            "vid": vid,
+            "vt": each['snippet']['title'],
+            "vd": isodate.parse_duration(each['contentDetails']['duration']),
+            "th": each['snippet']['thumbnails']['default']['url'],
+        } for each in response['items']]
+    except Exception as e:
+        return ("ERROR", e)
+
+def get_video_duration(vid):
+    request = get_api_connection().videos().list(
+        part="contentDetails",
+        id=vid,
+        fields="items(id,contentDetails(duration))"
+    )
+    response = request.execute()
+
+    return [{"id": each['id'],
+             "duration": each['contentDetails']['duration']
+             } for each in response['items']]
+
+# _____________________________________________________
+
+
+
+# def add_to_already_played(get_db_connection, v_id, v_name):
+#     print("PARAMETER", v_id, v_name)
+#
+#     sql = """ INSERT INTO already_played (video_id,video_name) VALUES (%s,%s)"""
+#     db_update = update_data_entry(get_db_connection, sql, (v_id, v_name))
+#     print(db_update)
+#
+#     return "OK"
+
+
+
+# def get_table_playing(get_db_connection):
+#     sql = """ SELECT * FROM playing"""
+#     connection = get_db_connection
+#     return connection.execute(sql).fetchall()
+#
+# def get_table_initial_entry(get_db_connection):
+#     sql = """ SELECT * FROM initial_entry ORDER BY updated_at ASC """
+#     return get_db_connection.execute(sql).fetchall()
+

9ec359b0f5c4c7e06963dcc5149446adabefae26
Shailesh Adhikari <shailesh@rasello.com>
2022-03-09 10:11:24 +0545

    changing request method and added remove

--- app.py
+++ app.py
@@ -48,18 +48,18 @@ def playing():
         result = table_playing(get_db_connection)
         return dict(result)
 
-@app.route("/search",methods=['POST'])
+@app.route("/search",methods=['GET'])
 def search():
-    if request.method == 'POST':
+    if request.method == 'GET':
         query = request.args.get('q')
         result = get_search_results(query)
         return Response(json.dumps(result,default=str),
                         mimetype='application/json')
 
 
-@app.route("/link",methods=['POST'])
+@app.route("/link",methods=['GET'])
 def link():
-    if request.method == 'POST':
+    if request.method == 'GET':
         query = request.args.get('link')
         result = get_video_name(return_vid(query))
         return Response(json.dumps(result,default=str),
@@ -107,6 +107,16 @@ def on_player_end():
 
     return result['id']
 
+@app.route("/remove", methods=[ 'GET'])
+def remove():
+    if request.method == 'GET':
+        id = request.args.get('id')
+        return remove_entry(
+            get_db_connection,
+            'DELETE FROM initial_entry WHERE video_id=%s',
+            id,
+        )
+
 # _______________________________________________________________
 
 @app.route("/", methods=['POST', 'GET'])
@@ -140,53 +150,3 @@ def search_add():
                                songl=table_data, status='Click To Add !',
                                playing = playing)
 #
-# @app.route("/addToDatabase")
-# def get_url_from_user():
-#     v_id = request.args.get('videoId')
-#     title = request.args.get('title')
-#     print('ADDED',v_id,title)
-#
-#     db_update = add_to_initial_entry(get_db_connection,v_id,title)
-#     print(db_update)
-#
-#     return redirect(url_for('search_add'))
-#
-#
-# @app.route("/endUpdate", methods=['POST', 'GET'])
-# def onPlayerEnd():
-#     print("Here at Last Update")
-#
-#     try:
-#         truncate(get_db_connection,'DELETE FROM playing')
-#     except Exception as e:
-#         print("Issue with Truncate !")
-#
-#     to_play = []
-#     initial_result = get_table_initial_entry(get_db_connection)
-#     if len(initial_result) == 0:
-#         videos = get_from_already_played(get_db_connection)
-#         if len(videos) <= 0:
-#             return 'TIME TO ADD VIDEOS ! !'
-#
-#         print(videos)
-#         to_play.extend((videos[0][0], videos[0][1]))
-#
-#         add_to_playing(get_db_connection, to_play[0], to_play[1])
-#
-#         result = get_table_playing(get_db_connection)
-#         to_return = [result[-1][1], result[-1][2]]
-#
-#         update_already_played(get_db_connection, to_play[0])
-#
-#     else:
-#         to_play.extend((initial_result[0][1], initial_result[0][4]))
-#         remove_entry(get_db_connection, 'DELETE FROM initial_entry WHERE id=%s', initial_result[0][0])
-#
-#         add_to_playing(get_db_connection,to_play[0],to_play[1])
-#
-#         result = get_table_playing(get_db_connection)
-#         to_return = [result[-1][1],result[-1][2]]
-#
-#         add_to_already_played(get_db_connection,to_return[0],to_return[1])
-#
-#     return to_return[0]

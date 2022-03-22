a799c9215417b12ad8c8670c8e745ab3cc408295
Shailesh Adhikari <shailesh@rasello.com>
2022-03-22 11:59:32 +0545

    Fix and Update views with model work

--- youtube_player/views.py
+++ youtube_player/views.py
@@ -1,84 +1,90 @@
-from flask import session, Response, request, render_template
+from flask import session, Response, request, render_template, jsonify
 from youtube_player import app
 from youtube_player.models import *
+from .decorators import logged_in
 
 @app.route("/auth/me",methods=['GET'])
-def index():
-    records = User.select()
+def me():
 
-    if session.get('logged_in'):
-        return Response(
-            response={
-                'username': session['username'],
-                'email': session['email'],
-                'token': records.where(User.email == session['email']).get().token
-            },
-            status=200,mimetype='application/json'
-        )
-    else:
-        return Response(
-            response={'error': 'Not logged in'},
-            status=401,mimetype='application/json'
-        )
+    if not session.get('logged_in'):
+        return jsonify({
+                'success':False,
+                'result':{'error':'Not logged in'}
+            })
+
+    records = User.query
+    return jsonify({
+        'success':True,
+        "result":{
+            'username': session['username'],'email': session['email'],
+            'token': records.where(User.email == session['email']).first().token
+        }
+        })
 
 @app.route('/auth/register',methods=['POST'])
 def register():
-    records = User.select()
+    records = User.query
 
     if request.method != 'POST':
-        return Response(
-            response={'error': 'Invalid request'},
-            status=401,mimetype='application/json'
-        )
-    username = request.form['username']
-    email = request.form['email']
-    password = request.form['password']
-    if records.where(User.email == email).exists():
-        return Response(
-            response={'error': 'Email already registered'},
-            status=401,mimetype='application/json'
-        )
-    new_user = User.create(username=username, email=email, password=password)
-    new_user.save()
-    return Response(
-        response={'success': 'Registered'},
-        status=200,mimetype='application/json'
-    )
+        return jsonify({
+            'success': False,
+            'result': {'error': 'Invalid request'}
+        })
+    data = request.get_json()
+    username = data['username']
+    email = data['email']
+    password = data['password']
+    if records.filter_by(email = email).first() is not None:
+        return jsonify({
+            'success': False,
+            'result':{'error': 'Email already registered'},
+        })
+    db.session.add(User(username=username, email=email, password=password))
+    db.session.commit()
+    return jsonify({
+        'success': True,
+        'result':{'message': 'Registered Successfully !'},
+    })
 
 @app.route("/auth/login",methods=['POST'])
 def login():
-    records = User.select()
+    records = User.query
 
     if request.method != 'POST':
-        return Response(
-            response={'error': 'Invalid request'},
-            status=401,mimetype='application/json'
-        )
-    email = request.form['email']
-    password = request.form['password']
-    login_user = records.where(User.email == email, User.password == password).get()
-    if login_user.exists():
-        session['logged_in'] = True
-        session['username'] = login_user['username']
-        session['email'] = email
-        return Response(
-            response={'success': 'Logged in'},
-            status=200,mimetype='application/json'
-        )
-    else:
-        return Response(
-            response={'error': 'Invalid credentials'},
-            status=401,mimetype='application/json'
-        )
+        return jsonify({
+            'success': False,
+            'result':{'error': 'Invalid request'},
+        })
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
 
-@app.route('/auth/logout')
+    login_user = records.filter_by(email = email, password = password).first()
+    if login_user is None:
+        return jsonify({
+            'success': False,
+            'result':{'error': 'Invalid credentials'}
+        })
+    session['logged_in'] = True
+    session['username'] = login_user.username
+    session['email'] = login_user.email
+    return jsonify({
+        'success': True,
+        "result": {
+            'username': session['username'], 'email': session['email'],
+            'token': login_user.cookie
+        }
+    })
+
+
+@app.route('/auth/logout',methods=['POST'])
+@logged_in
 def logout():
     session['logged_in'] = False
-    return Response(
-        response={'success': 'Logged out'},
-        status=200,mimetype='application/json'
-    )
+    return Response({
+        'success': True, 'result': {'message':'Logged out'}
+    })
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('../templates/404.html'), 404
+    return render_template('404.html',redirect='/',error=e), 404

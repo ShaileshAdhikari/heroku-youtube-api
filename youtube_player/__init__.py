a1c2c42ab426bdddcc9d9b89c87b2c5d09044730
Shailesh Adhikari <shailesh@rasello.com>
2022-03-22 12:00:12 +0545

    Added secret key to support login session

--- youtube_player/__init__.py
+++ youtube_player/__init__.py
@@ -10,6 +10,8 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.secret_key = 'a0b79ecc3ceca2372f865229192e90fd50f77099fc3868081af8a235f46f38ad'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 CORS(app)
 db = SQLAlchemy(app)

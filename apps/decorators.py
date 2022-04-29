240afdbcb8c29cc68a968888beca4ab0d19efdc8
Shailesh Adhikari <shailesh@rasello.com>
2022-04-29 09:46:58 +0545

    super user email change

--- apps/decorators.py
+++ apps/decorators.py
@@ -6,7 +6,7 @@ from flask_login import current_user
 def check_for_admin(f):
     @wraps(f)
     def decorated_func(*args, **kwargs):
-        return f(*args, **kwargs) if current_user.email == 'admin@admin.com' \
+        return f(*args, **kwargs) if current_user.email == 'admin@player.com' \
             else render_template("home/not-admin.html")
 
     return decorated_func

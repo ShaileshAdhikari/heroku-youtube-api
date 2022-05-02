2d162873f837e5bfca617b34fc2f9a8cba6c05b3
Shailesh Adhikari <shailesh@rasello.com>
2022-05-02 11:42:39 +0545

    Fix Database update issue

--- apps/config.py
+++ apps/config.py
@@ -22,7 +22,8 @@ class Config(object):
 
     # PostgreSQL database
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
 class ProductionConfig(Config):
     DEBUG = False
